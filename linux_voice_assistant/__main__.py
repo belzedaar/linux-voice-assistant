67c868660647786dfd5d7f23dd9209389781dac2
Belzedaar <6566875+belzedaar@users.noreply.github.com>
2026-02-22 17:23:34 +1100

    reduce fake activations

--- linux_voice_assistant/__main__.py
+++ linux_voice_assistant/__main__.py
@@ -430,7 +430,7 @@ def process_audio(state: ServerState, mic, block_size: int):
                         elif isinstance(wake_word, OpenWakeWord):
                             for oww_input in oww_inputs:
                                 for prob in wake_word.process_streaming(oww_input):
-                                    if prob > 0.5:
+                                    if prob > 0.85:
                                         activated = True
 
                         if activated and not state.muted:
